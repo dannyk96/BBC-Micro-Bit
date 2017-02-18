98a391e48f002d6f7026831216b5ca8c2b7da4c6
Joseph Kidger <joseph.kidger@gmail.com>
2017-02-18 20:58:45 +0000

    Cleaned up and added audio

--- Motion detector.py
+++ Motion detector.py
@@ -1,12 +1,15 @@
+import music
 from microbit import *
 dot28 = Image("99999:""99999:""99999:""99999:""99999")
-all_dots= [dot27, dot28, dot29, dot30,]
+notes = ['c8:1','c#']
 
 
 display.clear()
 
 while True:
-    if accelerometer.was_gesture('shake'):
+    if accelerometer.was_gesture('face up'):
         display.show(dot28)
+        while button_a.is_pressed() == False:
+            music.play(notes)
         sleep(1000)
         display.clear()
